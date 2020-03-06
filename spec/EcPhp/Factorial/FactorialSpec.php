645709c6761742ca26c24bb7d4ac9e572e1c7194
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-03-06 14:15:00 +0100

    Add better tests.

--- spec/EcPhp/Factorial/FactorialSpec.php
+++ spec/EcPhp/Factorial/FactorialSpec.php
@@ -14,11 +14,18 @@ class FactorialSpec extends ObjectBehavior
             ->shouldReturn(1);
     }
 
+    public function it_can_get_the_factorial_of_1()
+    {
+        $this
+            ->of(1)
+            ->shouldReturn(1);
+    }
+
     public function it_can_get_the_factorial_of_5()
     {
         $this
             ->of(5)
-            ->shouldBeInt();
+            ->shouldReturn(120);
     }
 
     public function it_is_initializable()
